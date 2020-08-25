2b0082de4bc93b2caaf7b62f1617e380d1fcc1e7
jiamy1227 <1049465510@qq.com>
2020-08-25 09:27:31 +0800

    Initial commit

---
+++ src/test/Test.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jiamy
+ * @Description :
+ * @Create on : 2020/8/24 12:05
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+
+        map.put("aaa","aaa");
+        map.put("bbb","bbb");
+        map.put("ccc","ccc");
+    }
+}
